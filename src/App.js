ce3c7aa2a018989b607e6cafa03b1d8530a53f17
Helen <filatovae047@gmail.com>
2025-03-21 17:19:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {testing} from './testing'
+
 
 function App() {
+  const [flags, setFlags] = useState(testing)
+  const removeFlags = (number) => {
+    let removeEachItem = flags.filter(name => name.number !==number)
+    setFlags(removeEachItem)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div >
+  {flags.map((element => {
+    const {number, name, picture} = element;
+    return(
+      <div>
+        <div className='container'>
+       <h2>{number} - {name}</h2>
+        </div>
+
+        <div className='container'>
+          <img src={picture} width="1000px" height="500px" alt="flags"/>
+        </div>
+
+        <div className='container'>
+          <button onClick={() => removeFlags(number)}>remove</button>
+        </div>
+      </div>
+    )
+  }))}
+
+  <div className='container'>
+    <button onClick={() => setFlags([])}>remove all flags</button>
+  </div>
+  </div>
+    
   );
 }
 
